7205ae0043f6d20f70ff400a2b805e53850f0f13
kulsoomansari <kulsoomansari412@gmail.com>
2021-01-05 13:00:07 +0500

    signup and login page working with conditions

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.listen(PORT, (req, res)=>{
     console.log('server is running on port:', PORT)
 })
-// app.get('/',(req, res)=>{
-//     // res.send('<h1>My first app</h1>')
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+let users = [
+    {name: 'kulsoom', email: 'kulsoom@gmail.com', pwd: 1234}
+]
+app.get('/',(req, res)=>{
+ res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 // app.get('/aboutus', (req, res)=>{
 //     // res.send('<h1>About us</h1>')
 //     res.sendFile(path.join(__dirname, 'public', 'aboutus.html'))
@@ -30,5 +32,24 @@ app.listen(PORT, (req, res)=>{
 
 app.use(express.static(path.join(__dirname, 'registration')))
 app.post('/signUp.html', (req, res)=>{
-    res.send(req.body)
+let {name, email, psw} = req.body  
+let found = users.some((item)=>item.email==email)
+if(found){
+    res.send('<h1>user already existed</h1>')
+}else{
+    users.push({name, email, psw, id: users.length +1})
+}
+res.redirect('/signin.html')
+    // res.send(req.body)
+})
+
+app.use(express.static(path.join(__dirname, 'registration')))
+app.post('/signin.html', (req, res)=>{
+    let {email, psw} = req.body
+    let foundsign = users.some((item)=>item.email == email && item.psw === psw)
+    if(foundsign){
+        res.send('<h1>Welocme to the homepage</h1>')
+    }else{
+        res.redirect('/signin.html')
+    }
 })
