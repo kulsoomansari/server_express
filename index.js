7a0eeaeb18d4da1a55dffe4a19268b903a7918aa
kulsoomansari <kulsoomansari412@gmail.com>
2021-01-13 12:13:14 +0500

    signup form

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const users = require('./Users')
 app.listen(PORT, (req, res)=>{
     console.log('server is running on port:', PORT)
